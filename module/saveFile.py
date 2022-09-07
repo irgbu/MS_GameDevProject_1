4c2d81ce32b845842d55f6aa2ff0ded0d42fdf2d
irgbu <irgbu1209@yonesi.ac.kr>
2022-09-07 13:01:32 +0900

    Shin_KB_2022/09/07_saveFile

---
+++ module/saveFile.py
@@ -0,0 +1,10 @@
+# MS_GameDev_Project1
+# Save-Load Module
+# Written by Shin, KB [irgbu1209@yonsei.ac.kr]
+
+class saveFile:
+    def __init__(self, name, time, turn, cutScene):
+        self.__name = name
+        self.__time = time
+        self.__turn = turn
+        self.__cutScene = cutScene
